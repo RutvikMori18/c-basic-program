44645089e2d937383db7a286d968812680e79257
RutvikMori18 <rutvikmori123@gmail.com>
2020-10-14 12:36:59 +0530

    ascii and palindrom example.

---
+++ ascii.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	char c1;
+	cout<<"enter charecter which you give ascii value: ";
+	cin>>c1;
+	
+	
+	cout<<"your charecter ascii value is : "<<int(c1);
+	 return 0;
+}
